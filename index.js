96c935f843860328f6fe16d2907adea11bf23d18
rick2785 <rjhrabowskie@gmail.com>
2023-08-17 23:08:16 -0500

    revert to working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const stripe = require("stripe")(process.env.API_LIVE_SKEY);
+// This is your test secret API key. 'sk_test_51NbiEjG1o8Hlx628ptBM1MgykKc1G8GVDAFfNXo1jiYVPxEKehxX1DdqORb8aGMhhOfrC54Etzr8g9dhtg8I4OTo00nS8xEQbS'
+// Live Key. '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'
+const stripe = require("stripe")('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');
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -20,7 +22,6 @@ app.post("/create-payment-intent", async (req, res) => {
   });
   console.log("Name:", items.Name);
   console.log("Email:", items.Email);
-
   const ephemeralKey = await stripe.ephemeralKeys.create(
     {customer: customer.id},
     {apiVersion: '2022-11-15'}
@@ -37,12 +38,16 @@ app.post("/create-payment-intent", async (req, res) => {
     }, 
   });
   
+  // res.send({
+  //   clientSecret: paymentIntent.client_secret,
+  // });
+  
   res.json({
     clientSecret: paymentIntent.client_secret,
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
-    publishableKey: process.env['API_LIVE_PKEY']
-  });
-});
+    publishableKey: 'pk_live_51NbiEjG1o8Hlx628Fydn2FtmPQcxlBvGzjQib0RNHfLTznumh2ePqtcBzKLRHeQSTK1McsrRsnn1GJGuBKqmKCS800YSQTL9xD'
+  }); // 'pk_test_51NbiEjG1o8Hlx628TkLNU2psfDEIhEcgSMab1ylvq5OaHW8dc6C60Cq6IF0BQIZyj0EJF1mLhhhCfZHSLIOHXjVF006iI7Y7Go'
+}); // 'pk_live_51NbiEjG1o8Hlx628Fydn2FtmPQcxlBvGzjQib0RNHfLTznumh2ePqtcBzKLRHeQSTK1McsrRsnn1GJGuBKqmKCS800YSQTL9xD'
 
-app.listen(process.env.PORT, () => console.log(`Node server listening on port ${process.env.PORT}!`));
+app.listen(7070, () => console.log("Node server listening on port 7070!"));
